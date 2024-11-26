69c798f71f53df7a6122a95aa286fa9e59fe38ec
weaam3 <weaamak3@gmail.com>
2024-11-26 23:32:48 +0000

    Blockchain

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -1,55 +1,67 @@
-// SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.26; 
 
-import "hardhat/console.sol";
+contract Crowdfunding{
+    address public owner;
+    address payable public receiver;    // accept payable addresses
+    uint public totalContributions;     // unit = unit256
+    uint public goalCertainAmount;      
+    bool public isFunded;          // Flag to track if the funds have been released          
 
-/**
- * @title Owner
- * @dev Set & change owner
- */
-contract Owner {
+    mapping(address => uint) public contributions; // stores the number of tokens each address has
 
-    address private owner;
+    // Event that logs each contribution
+    event ContributionReceived(address user, uint amount);
+    // Event that logs the release of funds to the recipient
+    event FundsReleased(address receiver, uint amount);
 
-    // event for EVM logging
-    event OwnerSet(address indexed oldOwner, address indexed newOwner);
 
-    // modifier to check if caller is owner
-    modifier isOwner() {
-        // If the first argument of 'require' evaluates to 'false', execution terminates and all
-        // changes to the state and to Ether balances are reverted.
-        // This used to consume all gas in old EVM versions, but not anymore.
-        // It is often a good idea to use 'require' to check if functions are called correctly.
-        // As a second argument, you can also provide an explanation about what went wrong.
-        require(msg.sender == owner, "Caller is not owner");
-        _;
+    // Initialize the contract
+    constructor(address payable _receiver, uint EtherGoalCertainAmount) {
+        owner = msg.sender; // the address of the genetrator of the contract becomes the owner
+        receiver = _receiver;   
+        goalCertainAmount = EtherGoalCertainAmount * 1 ether;  // convert the goal to Wei unit
+        isFunded = false;  // initially, funds are not released
     }
 
-    /**
-     * @dev Set contract deployer as owner
-     */
-    constructor() {
-        console.log("Owner contract deployed by:", msg.sender);
-        owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
-        emit OwnerSet(address(0), owner);
+    modifier onlyReceiver() {
+        require(msg.sender == receiver, "Not the beneficiary");
+        _; //Rest of the function body execution
+    }    
+
+
+    function theProcess() public payable {
+
+        require(!isFunded, "Funding is already completed");
+
+        // Get the contribution from the sender (users)
+        uint contribution = msg.value;
+        contributions[msg.sender] += contribution;
+        
+        totalContributions += contribution;
+
+        // Launch an event to log the users' contributions.
+        emit ContributionReceived(msg.sender, msg.value);
+
+        // Release the funds if the total contributions reach a certain amount
+        if (totalContributions >= goalCertainAmount) {
+            releaseFunds();
+        }
+
     }
 
-    /**
-     * @dev Change owner
-     * @param newOwner address of new owner
-     */
-    function changeOwner(address newOwner) public isOwner {
-        require(newOwner != address(0), "New owner should not be the zero address");
-        emit OwnerSet(owner, newOwner);
-        owner = newOwner;
-    }
+    function releaseFunds() public onlyReceiver{
 
-    /**
-     * @dev Return owner address 
-     * @return address of owner
-     */
-    function getOwner() external view returns (address) {
-        return owner;
+        require(totalContributions >= goalCertainAmount, "The goal has not been reached");
+        require(!isFunded, "The Funds have already been released");
+
+        // Set the flag to true indicating funds have been released
+        isFunded = true;
+
+        receiver.transfer(totalContributions);  // Transfer funds to the receiver.
+        emit FundsReleased(receiver, totalContributions);
+
     }
-} 
+    
+}
